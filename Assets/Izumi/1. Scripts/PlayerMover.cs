a8dd40ed0c0d7c7f11af6af05573e3b43c466564
rimrize <hw16a005@oecu.jp>
2019-01-17 11:52:37 +0900

    TilemapSystem
    MovePlayer

---
+++ Assets/Izumi/1. Scripts/PlayerMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    Vector2 vec = new Vector2(0.5f, 0.5f);
+    float vx;
+    float vy;
+    Rigidbody2D rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            vx = -1.25f;
+            vy = 0.0f;
+        } 
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            vx = 1.25f;
+            vy = 0.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            vx = 0.0f;
+            vy = 1.25f;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            vx = 0.0f;
+            vy = -1.25f;
+        }
+        rigidbody.velocity = new Vector2(vx, vy);
+	}
+}
---
+++ Assets/Izumi/1. Scripts/SceneMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMover : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("on");
+
+    }
+
+    void Update()
+    {
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("on");
+        SceneManager.LoadScene("Battle");
+    }
+}
